0029a8ed625e8f3a19c9fa2f7854b37210467210
connorhendrix <cghendrix@gmail.com>
2022-12-12 15:41:37 -0500

    Update deploy strategy

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -2,11 +2,22 @@
 
 swag init
 
+# build server
 docker buildx build \
+--build-arg COMMAND=server \
 --build-arg DB_USER=${DB_USER} \
 --build-arg DB_PASS=${DB_PASS} \
 --platform linux/amd64 \
 --push \
 -t gcr.io/nicklebackfacts/nbfactsserver .
+
+# build worker
+docker buildx build \
+--build-arg COMMAND=worker \
+--build-arg DB_USER=${DB_USER} \
+--build-arg DB_PASS=${DB_PASS} \
+--platform linux/amd64 \
+--push \
+-t gcr.io/nicklebackfacts/nbfactsworker .
 
 gcloud compute ssh --zone "us-west2-a"  --project "nicklebackfacts" --command "sudo systemctl start konlet-startup"  "nbfactsserver"
